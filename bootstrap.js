0ad4f35979dec73ad2344b379f27c2f9c24991ed
pedrojose.pena <pedrojose.pena@i3television.es>
2017-12-13 15:17:10 +0100

    Bugfix add cors in options

--- bootstrap.js
+++ bootstrap.js
@@ -6,56 +6,69 @@ const fs = require('fs');
 const index = fs.readFileSync('index.html', 'utf8');
 const ResponseBuilder = require('./app/ResponseBuilder');
 
-module.exports = function(app){
-    app.get('/*', (req, res) => {
-        const responseBuilder = new ResponseBuilder(res);
-        
-        const origionalUrl = req.originalUrl;
-        const requestedUrl = req.params[0];
-        const corsBaseUrl = '//' + req.get('host');
-        
-        console.info(req.protocol + '://' + req.get('host') + origionalUrl);
-        
-        if(requestedUrl == ''){
-            res.send(index);
-            return;
+module.exports = function(app) {
+  app.get('/*', (req, res) => {
+    const responseBuilder = new ResponseBuilder(res);
+
+    const origionalUrl = req.originalUrl;
+    const requestedUrl = req.params[0];
+    const corsBaseUrl = '//' + req.get('host');
+
+    console.info(req.protocol + '://' + req.get('host') + origionalUrl);
+
+    if (requestedUrl == '') {
+      res.send(index);
+      return;
+    }
+
+    request({
+      uri: requestedUrl,
+      resolveWithFullResponse: true,
+      headers: {
+        'User-Agent':
+          'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36'
+      }
+    })
+      .then(originResponse => {
+        responseBuilder
+          .addHeaderByKeyValue('Access-Control-Allow-Origin', '*')
+          .addHeaderByKeyValue('Access-Control-Allow-Credentials', false)
+          .addHeaderByKeyValue(
+            'Access-Control-Allow-Headers',
+            'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept'
+          )
+          .addHeaderByKeyValue(
+            'Access-Control-Allow-Methods',
+            'GET, POST, OPTIONS, PUT, PATCH, DELETE, CONNECT'
+          )
+          .build(originResponse.headers);
+        if (req.headers['rewrite-urls']) {
+          res.send(
+            converter
+              .convert(originResponse.body, requestedUrl)
+              .replace(requestedUrl, corsBaseUrl + '/' + requestedUrl)
+          );
+        } else {
+          res.send(originResponse.body);
         }
+      })
+      .catch(originResponse => {
+        responseBuilder
+          .addHeaderByKeyValue('Access-Control-Allow-Origin', '*')
+          .addHeaderByKeyValue('Access-Control-Allow-Credentials', false)
+          .addHeaderByKeyValue(
+            'Access-Control-Allow-Headers',
+            'X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept'
+          )
+          .addHeaderByKeyValue(
+            'Access-Control-Allow-Methods',
+            'GET, POST, OPTIONS, PUT, PATCH, DELETE, CONNECT'
+          )
+          .build(originResponse.headers);
 
-        request({
-            uri: requestedUrl,
-            resolveWithFullResponse: true,
-            headers: {
-                'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/48.0.2564.116 Safari/537.36'
-            }
-        })
-        .then(originResponse => {            
-            responseBuilder
-                .addHeaderByKeyValue('Access-Control-Allow-Origin', '*')
-                .addHeaderByKeyValue('Access-Control-Allow-Credentials', false)
-                .addHeaderByKeyValue('Access-Control-Allow-Headers', 'Content-Type')
-                .addHeaderByKeyValue('X-Proxied-By', 'cors-container')
-                .build(originResponse.headers);
-            if(req.headers['rewrite-urls']){
-                res.send(
-                    converter
-                        .convert(originResponse.body, requestedUrl)
-                        .replace(requestedUrl, corsBaseUrl + '/' + requestedUrl)
-                ); 
-            }else{
-                res.send(originResponse.body);                
-            }
-        })
-        .catch(originResponse => {
-            responseBuilder
-                .addHeaderByKeyValue('Access-Control-Allow-Origin', '*')
-                .addHeaderByKeyValue('Access-Control-Allow-Credentials', false)
-                .addHeaderByKeyValue('Access-Control-Allow-Headers', 'Content-Type')
-                .addHeaderByKeyValue('X-Proxied-By', 'cors-containermeh')
-                .build(originResponse.headers);
+        res.status(originResponse.statusCode || 500);
 
-            res.status(originResponse.statusCode || 500);
-            
-            return res.send(originResponse.message);
-        });
-    });
+        return res.send(originResponse.message);
+      });
+  });
 };
